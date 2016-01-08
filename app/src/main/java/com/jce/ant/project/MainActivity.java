f268549c0b37df6abb7e6851f6a2689686e9e903
anttross <anttross@gmail.com>
2016-01-08 03:52:38 +0200

    first activity: preview only second activity: insert picture

--- app/src/main/java/com/jce/ant/project/MainActivity.java
+++ app/src/main/java/com/jce/ant/project/MainActivity.java
@@ -1,69 +1,32 @@
 package com.jce.ant.project;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SELECT_PICTURE = 1;
-    private String selectedImagePath;
+    View title;
+    MainView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.Button01)
-                .setOnClickListener(new View.OnClickListener() {
+        title=(TextView)findViewById(R.id.chooseFormat);
 
-                    public void onClick(View arg0) {
+        view = new MainView(this);
+        view = (MainView) findViewById(R.id.mainView);
+        view.setOnClickListener(new View.OnClickListener() {
 
-                        // in onCreate or any event where your want the user to
-                        // select a file
-                        Intent intent = new Intent();
-                        intent.setType("image/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent,
-                                "Select Picture"), SELECT_PICTURE);
-                    }
-                });
-    }
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EditActivity.class));
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
             }
-        }
+        });
     }
-
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-        // just some safety built in
-        if( uri == null ) {
-            // TODO perform some logging or show user feedback
-            return null;
-        }
-        // try to retrieve the image from the media store first
-        // this will only work for images selected from gallery
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        // this is our fallback here
-        return uri.getPath();
-    }
-
 }
