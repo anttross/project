1036ff8df5ffe1a976a12d010f1724b072fd873f
anttross <anttross@gmail.com>
2016-01-10 22:33:47 +0200

    pic size

---
+++ app/src/main/java/com/jce/ant/project/EditView.java
@@ -0,0 +1,56 @@
+package com.jce.ant.project;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Ant on 08-Jan-16.
+ */
+public class EditView extends View {
+    Paint paint = new Paint();
+    private Path path;
+    int border=40;
+
+    private void init (AttributeSet attrs, int defStyle){
+        paint = new Paint();
+        // xr=yr=x=y = 0;
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        path = new Path();
+    }//init
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.parseColor("#CC333333"));
+        int w = getWidth()-border;
+        int h = getHeight()-border;
+        int side= (w<h?w:h);
+
+        //canvas.drawRect((w-side)/2, 0, (w-side)/2+side, side*3/2, paint);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(border);
+        canvas.drawRect((w - side) / 2 + (border / 2), border / 2, (w - side) / 2 + side+(border/2), side+(border/2), paint);
+
+    }
+
+
+
+    public EditView(Context context) {
+        super(context);
+    }
+
+    public EditView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/jce/ant/project/MainView.java
@@ -0,0 +1,54 @@
+package com.jce.ant.project;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Ant on 07-Jan-16.
+ */
+public class MainView extends View {
+
+    Paint paint = new Paint();
+    private Path path;
+
+
+    private void init (AttributeSet attrs, int defStyle){
+        paint = new Paint();
+       // xr=yr=x=y = 0;
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        path = new Path();
+    }//init
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.parseColor("#333333"));
+        int w = getWidth();
+        int h = getHeight()/3;
+        int side= (w<h?w:h)*2/3;
+
+        canvas.drawRect((w-side)/2, 0, (w-side)/2+side, side, paint);
+        canvas.drawRect((w-side*3/2)/2, h, (w-side*3/2)/2+(side*3/2), h+side, paint);
+        canvas.drawRect((w-side)/2, h*2,(w-side)/2+side ,h*2+side*3/2, paint);
+    }
+
+    public MainView(Context context) {
+        super(context);
+    }
+
+    public MainView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MainView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
